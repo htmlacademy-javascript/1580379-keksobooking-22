7a61b6945cdd0f9e31925aa6ecd476236402d285
Антон <opgx.46@gmail.com>
2021-02-03 22:01:26 +0300

    Добавление функций

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+const getRandomInteger = function (min, max) {
+  if (min >= 0 && max > min) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  return ('используйте положительные числа по нарастанию "от" меньшего "до" большего');
+}
+
+
+const getRandomNumber = function (min, max, fix) {
+  if (min >= 0 && max > min) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return ((Math.random() * (max - min + 1)) + min).toFixed(fix);
+  }
+  return ('используйте положительные числа по нарастанию "от" меньшего "до" большего');
+}
+
