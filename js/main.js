2796a673dfd8c5c1c3473eb4b5bdfbb5cef5add2
Антон <opgx.46@gmail.com>
2021-02-17 23:25:33 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,24 +1,58 @@
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+/* eslint-disable */
 
-const getRandomInteger = function (min, max) {
+
+function getRandomInteger(min, max) {
   if (min >= 0 && max > min) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+    return (Math.floor(Math.random() * (max - min + 1)) + min);
   }
-  return ('используйте положительные числа по нарастанию "от" меньшего "до" большего');
+  return 0;
 }
 
-getRandomInteger(10, 25);
-
-
-const getRandomNumber = function (min, max, fix) {
+function getRandomNumber(min, max, fix) {
   if (min >= 0 && max > min) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return ((Math.random() * (max - min + 1)) + min).toFixed(fix);
+    return Number(((Math.random() * (max - min + 1)) + min).toFixed(fix));
   }
-  return ('используйте положительные числа по нарастанию "от" меньшего "до" большего');
+  return NaN;
 }
 
-getRandomNumber(43, 90);
+const TYPES = ['palace', 'flat', 'house', 'bungalow'];
+
+const CHEKS = ['12:00', '13:00', '14:00'];
+
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+
+const PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
+
+function getRandomAd() {
+  return {
+    author: {
+      avatar: 'img/avatars/user0' + getRandomInteger(1, 8) + '.png',
+    },
+    offer: {
+      title: 'super title',
+      // address,
+      price: getRandomInteger(1, 100000),
+      type: TYPES[getRandomInteger(0, TYPES.length - 1)],
+      rooms: getRandomInteger(1, 10),
+      guests: getRandomInteger(1, 10),
+      checkin: CHEKS[getRandomInteger(0, CHEKS.length - 1)],
+      checkout: CHEKS[getRandomInteger(0, CHEKS.length - 1)],
+      //не понимаю как миновать повторение значений
+      //features: new Array(getRandomInteger(0, FEATURES.length - 1)).fill(null),
+      description: 'stylno, modno, molodejno',
+      //photos: new Array(getRandomInteger(1, 50)).fill(null),
+    },
+    location: {
+      x: getRandomNumber(35.65, 35.7, 5),
+    }
+  };
+};
+
+console.log(getRandomAd());
