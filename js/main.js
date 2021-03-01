5a576f8ea552580125644a90c4f42ac63d7bf8a6
Антон <opgx.46@gmail.com>
2021-03-01 15:54:43 +0300

    Исправленный Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,24 +1,7 @@
-
-const getRandomInteger = (min, max) => {
-  if (min >= 0 && max > min) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return (Math.floor(Math.random() * (max - min + 1)) + min);
-  }
-
-  return 0;
-}
-
-const getRandomNumber = (min, max, fix) => {
-  if (min >= 0 && max > min) {
-    return Number(((Math.random() * (max - min + 1)) + min).toFixed(fix));
-  }
-  return NaN;
-}
-
+/* eslint-disable */
 const TYPES = ['palace', 'flat', 'house', 'bungalow'];
 
-const CHEKS = ['12:00', '13:00', '14:00'];
+const CHECKS = ['12:00', '13:00', '14:00'];
 
 const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 
@@ -28,13 +11,37 @@ const PHOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
 ];
 
+const getRandomInteger = (min, max) => {
+  if (min >= 0 && max > min) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return (Math.floor(Math.random() * (max - min + 1)) + min);
+  }
+
+  return 0;
+};
+
+const getRandomNumber = (min, max, fix) => {
+  if (min >= 0 && max > min) {
+    return Number(((Math.random() * (max - min + 1)) + min).toFixed(fix));
+  }
+  return NaN;
+};
+
+
+const getRandomArrayItem = (items) => items[getRandomInteger(0, items.length - 1)];
+
+const getRandomArrayLength = (items) => items.slice(0, getRandomInteger(1, items.length - 1));
+
+const generateAdverts = (count) => {
+  return new Array(count).fill(null).map(getRandomAdvert);
+};
+
 const getRandomAdvert = () => {
   const location = {
     x: getRandomNumber(35.65, 35.7, 5),
     y: getRandomNumber(139.7, 139.8, 5),
   };
-
-  const getRandomEllement = arrayName => arrayName[getRandomInteger(0, arrayName.length - 1)];
 
   return {
     author: {
@@ -44,17 +51,41 @@ const getRandomAdvert = () => {
       title: 'super title',
       address: location.x + ', ' + location.y,
       price: getRandomInteger(1, 100000),
-      type: getRandomEllement(TYPES),
+      type: getRandomArrayItem(TYPES),
       rooms: getRandomInteger(1, 10),
       guests: getRandomInteger(1, 10),
-      checkin: getRandomEllement(CHEKS),
-      checkout: getRandomEllement(CHEKS),
-      features: FEATURES.slice(0, getRandomInteger(1, FEATURES.length - 1)),
+      checkin: getRandomArrayItem(CHECKS),
+      checkout: getRandomArrayItem(CHECKS),
+      features: getRandomArrayLength(FEATURES),
       description: 'stylno, modno, molodejno',
-      photos: PHOTOS.slice(0, getRandomInteger(1, PHOTOS.length - 1)),
+      photos: getRandomArrayLength(PHOTOS),
     },
     location,
   };
 };
 
-new Array(10).fill(null).map(getRandomAdvert);
+
+console.log(generateAdverts(10));
+
+//.map(function () {
+//return getRandomAd();
+// return return getRandomAd();(index + 1) // 1, 2, 3
+//});
+
+//console.log(similarObjects);
+
+// См. https://up.htmlacademy.ru/javascript/22/demos/4929#5
+// Нижней границей диапазона будет 0,
+// а верхней — максимальный индекс, который можно найти по формуле длина массива - 1,
+// потому что индексы нумеруются с нуля.
+
+// const items = [1, 2, 3] // index: 0 (1), 1 (2), 2 (3) = 2, items.length (3)
+
+// См. getRandomArrayElement
+// https://up.htmlacademy.ru/javascript/22/demos/4929#15
+
+// Нужно заменить декл. function на стрелочную функцию
+
+
+// данные в объектах хранят рекламное объявление (по ТЗ)
+// далее рекл. объеялвение будет отображаться (рендерится) в метке (pin) и в карточке (card)
